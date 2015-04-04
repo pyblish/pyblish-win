eb25f315da006135a207ab7dfe787c2389e62825
Marcus Ottosson <konstruktion@gmail.com>
2015-04-04 21:50:27 +0100

    Adding descriptions to installation scripts

--- install.bat
+++ install.bat
@@ -1,3 +1,7 @@
+:: Install Pyblish for Windows
+::
+:: Run this after cloning to fetch essential libraries.
+
 @echo off
 pushd %~dp0
 git submodule update --init --recursive
--- update.bat
+++ update.bat
@@ -1,3 +1,7 @@
+:: Update to the latest version of Pyblish for Windows
+::
+:: This update applies to all of Pyblish and it's dependencies.
+
 @echo off
 pushd %~dp0
 git reset --hard
