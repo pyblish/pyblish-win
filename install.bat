f65044231a05c16b0aa0f7c3d1afac850f946d3f
Marcus Ottosson <konstruktion@gmail.com>
2015-04-21 14:01:03 +0100

    Automatically installing integrations onto local system

--- install.bat
+++ install.bat
@@ -6,9 +6,16 @@
 :: wiki: https://github.com/pyblish/pyblish/wiki
 
 @echo off
+
+echo "Downloading Pyblish.."
 pushd %~dp0
 git submodule update --init --recursive
 popd
 
 :: Add to PYTHONPATH
+echo "Installing to local system.."
 setx PYTHONPATH %~dp0python;%PYTHONPATH%
+
+:: Install integrations
+echo "Installing integrations.."
+setx PYTHONPATH %~dp0lib/pyblish-suite/pyblish-maya/pyblish_maya/pythonpath;%~dp0lib/pyblish-suite/pyblish-nuke/pyblish_nuke/nuke_path;%PYTHONPATH%
