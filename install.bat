7a13f750013ebeeaab6b8d648262eed4485db911
Marcus Ottosson <konstruktion@gmail.com>
2015-04-21 13:23:43 +0100

    Updating install.bat

--- install.bat
+++ install.bat
@@ -1,8 +1,14 @@
 :: Install Pyblish for Windows
 ::
-:: Run this after cloning to fetch essential libraries.
+:: Run this after cloning to fetch essential libraries and add relevant
+:: directories to your local PYTHONPATH. For further information about
+:: installation into a distributed environment, see the main Pyblish
+:: wiki: https://github.com/pyblish/pyblish/wiki
 
 @echo off
 pushd %~dp0
 git submodule update --init --recursive
 popd
+
+:: Add to PYTHONPATH
+setx PYTHONPATH %~dp0python;%PYTHONPATH%
