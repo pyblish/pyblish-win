f8b1eb44202e148d4f990a9cfe36ae1525485b73
Marcus Ottosson <konstruktion@gmail.com>
2015-08-04 17:02:44 +0100

    Fixed test.bat

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 @echo off
-pushd %~dp0lib\pyblish-suite\pyblish
-"%~dp0lib/Python27/python.exe" %~dp0lib/pyblish-suite/pyblish/run_testsuite.py
+pushd %~dp0lib\pyblish-x\modules\pyblish
+"%~dp0lib/Python27/python.exe" "%~dp0lib/pyblish-x/modules/pyblish/run_testsuite.py"
 popd
 pause
