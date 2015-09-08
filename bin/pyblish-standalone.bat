713ec9ce00a83434ae37e4d9b40ecb74edcd5ca4
Marcus Ottosson <konstruktion@gmail.com>
2015-09-08 09:05:54 +0100

    Improving pyblish standalone binary

--- bin/pyblish-standalone.bat
+++ bin/pyblish-standalone.bat
@@ -1,2 +1,4 @@
 @echo off
+set PYBLISH_QML_CONSOLE=1
+set PYBLISHSTANDALONEBATCH=1  :: Signal that it can't be gracefully shutdown
 "%~dp0python" "%~dp0_launcher.py" "%~dp0\" pyblish_standalone %*
--- bin/python.bat
+++ bin/python.bat
@@ -1,3 +1,3 @@
 @echo off
 set PYTHONPATH=%~dp0..\pythonpath;%~dp0..\lib\python-qt5;%PYTHONPATH%
-"%~dp0..\lib\Python27\python.exe" %*
+"%~dp0..\lib\Python27\python.exe" -u %*
