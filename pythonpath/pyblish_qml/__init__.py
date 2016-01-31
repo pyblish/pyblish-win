25bc82976c995153122b425e97fc28f7cd6c861e
Marcus Ottosson <konstruktion@gmail.com>
2016-01-31 18:19:28 +0000

    Avoiding name clash of _util.py

--- pythonpath/pyblish_qml/__init__.py
+++ pythonpath/pyblish_qml/__init__.py
@@ -1,7 +1,7 @@
 import os
 import imp
 
-util = imp.load_source("_util", os.path.join(__file__, "..", "..", "_util.py"))
+util = imp.load_source("__pyblish_util", os.path.join(__file__, "..", "..", "__pyblish_util.py"))
 util.wrap_module(__name__.replace("_", "-"))
 util.install_dependencies()
 util.install_path()
